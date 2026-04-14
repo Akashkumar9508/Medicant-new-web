c7262c10c1b7ad69b8a53d72428b34e50855837a
Akashkumar9508 <akashbca9508@gmail.com>
2026-04-14 16:22:30 +0530

    temp

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-<<<<<<< HEAD
-=======
-import { componentTagger } from "lovable-tagger";
->>>>>>> 8d3f2ab0783f24e6701044a02bd1a32cb91a94d4
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -15,10 +11,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-<<<<<<< HEAD
-=======
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
->>>>>>> 8d3f2ab0783f24e6701044a02bd1a32cb91a94d4
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
